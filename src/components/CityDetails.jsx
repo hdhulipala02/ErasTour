7b9fb93613855819756bc87551f9d08c78f28a2d
ashaaare <ashaaare01@gmail.com>
2023-10-25 19:25:08 -0500

    added surprise songs

--- src/components/CityDetails.jsx
+++ src/components/CityDetails.jsx
@@ -5,6 +5,15 @@ const CityDetails = ({ city, onClose }) => (
     <div className='city-popup'>
         <h1 className='city-name'>{city.name} Statistics</h1>
         <p>Days spent: {city.days}</p>
+        <div>
+        <h3>Surprise Songs:</h3>
+        {city.shows.map((show, index) => (
+            <div key={index}>
+            <strong>{show.date}:</strong> {show.surpriseSongs.join(' and ')}
+            </div>
+        ))}
+        </div>
+        <br></br>
         <button className='city-button' onClick={onClose}>Close</button>
     </div>
 );
