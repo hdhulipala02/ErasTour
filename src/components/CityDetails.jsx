b0b58cb4301904235151f70b2dbd9cfb91b20062
ashaaare <ashaaare01@gmail.com>
2023-11-24 15:12:07 -0600

    worked on popup

--- src/components/CityDetails.jsx
+++ src/components/CityDetails.jsx
@@ -2,55 +2,73 @@ import React, { useState } from 'react';
 import '../style.css';
 
 const CityDetails = ({ city, onClose }) => {
-    const [selectedNight, setSelectedNight] = useState(null);
+  const [selectedNight, setSelectedNight] = useState(null);
 
-    const handleNightClick = (night) => {
-        setSelectedNight(night);
-    };
+  const handleNightClick = (night) => {
+    setSelectedNight(night);
+  };
 
-    const nightsToShow = Math.min(city.days, 2);
+  const nightsToShow = Math.min(city.days, 2);
 
-    return (
-        <div className='city-popup'>
-            <h1 className='city-name'>{city.name} Statistics</h1>
-            <p>Days spent: {city.days}</p>
+  return (
+    <div className='city-popup'>
+      <h1 className='city-name'>{city.name} Statistics</h1>
+      <p>Days spent: {city.days}</p>
+      {selectedNight && (
+        <div>
+          <h3>Surprise Songs:</h3>
+          {selectedNight === 'Nights 3+' && city.name === 'Los Angeles' ? (
             <div>
-                <h3>Surprise Songs:</h3>
-                {city.shows.map((show, index) => (
-                    <div key={index}>
-                        <strong>{show.date}:</strong> {show.surpriseSongs.join(' and ')}
-                    </div>
-                ))}
-            </div>
-            <br />
-            <div className="night-buttons">
-                {[...Array(nightsToShow).keys()].map((night) => (
-                    <button className='night-button' key={night} onClick={() => handleNightClick(night + 1)}>
-                        Night {night + 1}
-                    </button>
-                ))}
-                {city.days >= 3 && (
-                    <button className='night-button' onClick={() => handleNightClick('Nights 3+')}>
-                        Nights 3+
-                    </button>
-                )}
-            </div>
-            {selectedNight && (
-                <div>
-                    {/* Render content for selected night */}
-                    {selectedNight === 'Nights 3+' ? (
-                        <div>Content for Nights 3+</div>
-                    ) : (
-                        <div>Content for Night {selectedNight}</div>
-                    )}
+              {city.shows.slice(2).map((show, index) => (
+                <div key={index}>
+                  <div><strong>{show.date}:</strong></div>
+                  {show.surpriseSongs.map((song, index) => (
+                    <div key={index}>{song}</div>
+                  ))}
                 </div>
-            )}
-            <br />
-            <button className='city-button' onClick={onClose}>
-                Close
-            </button>
+              ))}
+            </div>
+          ) : (
+            <div>
+              {selectedNight === 'Nights 3+' && city.name !== 'Los Angeles' ? (
+                <div>
+                  {city.shows.slice(2).map((show, index) => (
+                    <div key={index}>
+                      {show.surpriseSongs.map((song, index) => (
+                        <div key={index}>{song}</div>
+                      ))}
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <div>
+                  {city.shows[selectedNight - 1]?.surpriseSongs.map((song, index) => (
+                    <div key={index}>{song}</div>
+                  ))}
+                </div>
+              )}
+            </div>
+          )}
         </div>
-    );
+      )}
+      <br />
+      <div className='night-buttons'>
+        {city.shows.slice(0, nightsToShow).map((show, night) => (
+          <button className='night-button' key={night + 1} onClick={() => handleNightClick(night + 1)}>
+            {show.date}
+          </button>
+        ))}
+        {city.days >= 3 && (
+          <button className='night-button' onClick={() => handleNightClick('Nights 3+')}>
+            {city.name === 'Los Angeles' ? 'August 5+' : city.shows[2].date}
+          </button>
+        )}
+      </div>
+      <button className='city-button' onClick={onClose}>
+        Close
+      </button>
+    </div>
+  );
 };
 
 export default CityDetails;
